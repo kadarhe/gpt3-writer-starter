dcb81c0e778df3981c8daff50d4f280ddd44f902
kadarhe <kadarhe@usc.edu>
2022-12-17 16:28:54 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,6 @@ import buildspaceLogo from '../assets/buildspace-logo.png';
 const Home = () => {
   return (
     <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
@@ -16,6 +13,10 @@ const Home = () => {
           <div className="header-subtitle">
             <h2>insert your subtitle here</h2>
           </div>
+        </div>
+        {/* Add this code here*/}
+        <div className="prompt-container">
+          <textarea placeholder="start typing here" className="prompt-box" />
         </div>
       </div>
       <div className="badge-container grow">
@@ -33,5 +34,6 @@ const Home = () => {
     </div>
   );
 };
+
 
 export default Home;
